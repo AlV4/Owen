ee06c296520c0faccf5a5b2b3d994a8dd1f84055
AlV4 <alex187b@bk.ru>
2016-03-07 22:50:32 +0200

    3 windows completed

--- src/com/plcdev/ui/CardLayoutDemo.java
+++ src/com/plcdev/ui/CardLayoutDemo.java
@@ -11,13 +11,13 @@ public class CardLayoutDemo implements ItemListener {
     JPanel windows; //a panel that uses CardLayout
     final static String MAINPANEL = "Start main panel";
     final static String NO_PRG_PANEL = "Panel with no programs";
-    final static String USER_PRODUST_CHOOZER = "User product choozer";
+    final static String USER_PRODUST_CHOOSER = "User product chooser";
     final static String USER_INSTRUCTIONS = "User instructions";
     final static String USER_IN_PROGRESS = "User info in progress";
     final static String USER_OPER_COMPLETED = "User operation completed";
     protected JLabel text;
-    protected JButton user;
-    protected JButton engineer;
+    protected JButton leftButton;
+    protected JButton rightButton;
 
     public void addComponentToPane(Container pane) {
         //Put the JComboBox in a JPanel to get a nicer look.
@@ -25,7 +25,7 @@ public class CardLayoutDemo implements ItemListener {
         String comboBoxItems[] = {
                 MAINPANEL,
                 NO_PRG_PANEL,
-                USER_PRODUST_CHOOZER,
+                USER_PRODUST_CHOOSER,
                 USER_INSTRUCTIONS,
                 USER_IN_PROGRESS,
                 USER_OPER_COMPLETED
@@ -38,6 +38,7 @@ public class CardLayoutDemo implements ItemListener {
         windows = new JPanel(new CardLayout());
         windows.add(mainPanel(), MAINPANEL);
         windows.add(noProductPanel(), NO_PRG_PANEL);
+        windows.add(productChooser(), USER_PRODUST_CHOOSER);
 
         pane.add(comboBoxPane, BorderLayout.PAGE_START);
         pane.add(windows, BorderLayout.CENTER);
@@ -56,43 +57,56 @@ public class CardLayoutDemo implements ItemListener {
 
         buttonsSet("START", "CONFIG MODE", "start.png", "settings.png", NO_PRG_PANEL, MAINPANEL);
 
-        panel.add(user, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        panel.add(engineer, new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        panel.add(leftButton, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        panel.add(rightButton, new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
         return panel;
     }
 
     private JPanel noProductPanel(){
         JPanel panel = new JPanel(new GridBagLayout());
-        textSet("Sorry, there is no available programs at the moment!");
+        textSet("Sorry, no available programs at the moment!");
         panel.add(text, new GridBagConstraints(
                 0, 0, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.VERTICAL, new Insets(0, 0, 0, 0), 0, 0));
 
         buttonsSet("BACK", "CONFIG MODE", "back.png", "settings.png", MAINPANEL, MAINPANEL);
 
-        panel.add(user, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        panel.add(engineer, new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        panel.add(leftButton, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        panel.add(rightButton, new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
         return panel;
     }
 
-    private JPanel createProductChoozer(){
+    private JPanel productChooser(){
         JPanel panel = new JPanel(new GridBagLayout());
-        textSet("Select, a program from the list below.");
+        textSet("Select a program from the list below.");
         panel.add(text, new GridBagConstraints(
                 0, 0, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.VERTICAL, new Insets(0, 0, 0, 0), 0, 0));
 
-        buttonsSet("BACK", "CONFIG MODE", "back.png", "settings.png", MAINPANEL, MAINPANEL);
+        String comboBoxItems[] = {
+                "First program",
+                "Second",
+                "So on",
+                USER_INSTRUCTIONS,
+                USER_IN_PROGRESS,
+                USER_OPER_COMPLETED
+        };
 
-        panel.add(user, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        panel.add(engineer, new GridBagConstraints(1, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        JComboBox<String> cb = new JComboBox<>(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+        panel.add(cb, new GridBagConstraints(0, 1, 2, 1, 1, 1, GridBagConstraints.ABOVE_BASELINE, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        buttonsSet("BACK", "NEXT", "back.png", "next.png", MAINPANEL, MAINPANEL);
+
+        panel.add(leftButton, new GridBagConstraints(0, 2, 1, 1, 1, 1, GridBagConstraints.ABOVE_BASELINE, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+        panel.add(rightButton, new GridBagConstraints(1, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
         return panel;
     }
 
     private void buttonsSet(String button1, String button2,String image1, String image2, final String actionPanel1, final String actionPanel2) {
-        user = new JButton(button1);
-        user.setVerticalTextPosition(SwingConstants.BOTTOM);
-        user.setHorizontalTextPosition(SwingConstants.CENTER);
-        user.setIcon(new ImageIcon(image1));
-        user.addActionListener(new ActionListener() {
+        leftButton = new JButton(button1);
+        leftButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        leftButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        leftButton.setIcon(new ImageIcon(image1));
+        leftButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout) (windows.getLayout());
@@ -100,11 +114,11 @@ public class CardLayoutDemo implements ItemListener {
             }
         });
 
-        engineer = new JButton(button2);
-        engineer.setVerticalTextPosition(SwingConstants.BOTTOM);
-        engineer.setHorizontalTextPosition(SwingConstants.CENTER);
-        engineer.setIcon(new ImageIcon(image2));
-        engineer.addActionListener(new ActionListener() {
+        rightButton = new JButton(button2);
+        rightButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        rightButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        rightButton.setIcon(new ImageIcon(image2));
+        rightButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout) (windows.getLayout());
